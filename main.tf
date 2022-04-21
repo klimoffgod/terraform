2f79c6698ba3ad9614a28c7a302407296409467f
root <root@bus.local>
2022-04-21 17:54:51 +0300

    del log

--- main.tf
+++ main.tf
@@ -1,11 +1,4 @@
 terraform {
-  pm_log_enable = true
-  pm_log_file   = "terraform-plugin-proxmox.log"
-  pm_debug      = true
-  pm_log_levels = {
-    _default    = "debug"
-    _capturelog = ""
-  }
   required_version = ">= 0.13"
   required_providers {
     proxmox = {
